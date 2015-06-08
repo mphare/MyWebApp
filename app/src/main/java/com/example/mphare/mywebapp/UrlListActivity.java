87fdc44f69401c73d5c20af832c45ccbe2389467
Michael Hare <mphare@whitehare.com>
2015-06-07 22:14:02 -0500

    new activity

--- app/src/main/java/com/example/mphare/mywebapp/UrlListActivity.java
+++ app/src/main/java/com/example/mphare/mywebapp/UrlListActivity.java
@@ -1,35 +1,25 @@
 package com.example.mphare.mywebapp;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class SettingsActivity extends ActionBarActivity
+public class UrlListActivity extends ActionBarActivity
 {
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_settings);
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-  }
-
-  public void onChangeUrl(View view)
-  {
-    Intent intent = new Intent(this, SettingsActivity.class);
-    startActivity(intent);
-
+    setContentView(R.layout.activity_url_list);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu)
   {
     // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_settings, menu);
+    getMenuInflater().inflate(R.menu.menu_url_list, menu);
     return true;
   }
 
