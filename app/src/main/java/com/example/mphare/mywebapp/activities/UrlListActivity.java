c6474d4f83a43f03dba02adfd47108fa5c179e45
Michael Hare <mphare@whitehare.com>
2015-06-13 22:55:31 -0500

    List Activity

--- app/src/main/java/com/example/mphare/mywebapp/activities/UrlListActivity.java
+++ app/src/main/java/com/example/mphare/mywebapp/activities/UrlListActivity.java
@@ -1,11 +1,17 @@
 package com.example.mphare.mywebapp.activities;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.mphare.mywebapp.R;
+import com.example.mphare.mywebapp.StableArrayAdapter;
+
+import java.util.ArrayList;
 
 public class UrlListActivity extends ActionBarActivity
 {
@@ -15,6 +21,35 @@ public class UrlListActivity extends ActionBarActivity
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_url_list);
+
+    final ListView listView = (ListView) findViewById(R.id.url_list_view);
+    String[] values = new String[]{"192.168.1.13:8080", "192.168.43.141:8080"};
+    final ArrayList<String> list = new ArrayList<String>();
+
+    for (int i = 0; i < values.length; ++i)
+    {
+      list.add(values[i]);
+    }
+    final StableArrayAdapter adapter = new StableArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+    listView.setAdapter(adapter);
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+    {
+      @Override
+      public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
+      {
+        final String item = (String) parent.getItemAtPosition(position);
+        view.animate().setDuration(2000).alpha(0).withEndAction(new Runnable()
+        {
+          @Override public void run()
+          {
+            list.remove(item);
+            adapter.notifyDataSetChanged();
+            view.setAlpha(1);
+          }
+        });
+      }
+    });
+
   }
 
   @Override
@@ -42,3 +77,5 @@ public class UrlListActivity extends ActionBarActivity
     return super.onOptionsItemSelected(item);
   }
 }
+
+
